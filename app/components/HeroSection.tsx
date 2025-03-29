793843e65fc5246c6032041bbcff8041091a48f1
Aarush <aarushj1300@gmail.com>
2025-03-29 15:43:28 -0400

    Img optimizations

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import { ChevronRight, CreditCard, Check, Smartphone } from 'lucide-react';
 
 export default function HeroSection() {
